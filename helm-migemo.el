3578bc968206480297144a5250e4d8d7d0459187
Syohei YOSHIDA <syohex@gmail.com>
2015-10-01 15:12:04 +0900

    Use helm-core instead of helm

--- helm-migemo.el
+++ helm-migemo.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Yuhei Maeda <yuhei.maeda_at_gmail.com>
 ;; Version: 1.20
 ;; Package-version: 1.20
-;; Package-Requires: ((helm "1.7.8") (migemo "1.9") (cl-lib "0.5"))
+;; Package-Requires: ((helm-core "1.7.8") (migemo "1.9") (cl-lib "0.5"))
 ;; Created: 2009-04-13 
 ;; Keywords: matching, convenience, tools, i18n
 ;; URL: https://github.com/emacs-jp/helm-migemo

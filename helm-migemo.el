66c6a19d07c6a385daefd2090d0709d26b608b4e
myuhe <yuhei.maeda@gmail.com>
2015-10-10 12:56:20 +0900

    Add package requires
    `with-eval-after-load` is introduced in Emacs 24.4.

--- helm-migemo.el
+++ helm-migemo.el
@@ -5,7 +5,7 @@
 ;; Author: rubikitch <rubikitch@ruby-lang.org>
 ;; Maintainer: Yuhei Maeda <yuhei.maeda_at_gmail.com>
 ;; Version: 1.22
-;; Package-Requires: ((helm-core "1.7.8") (migemo "1.9") (cl-lib "0.5"))
+;; Package-Requires: ((emacs "24.4") (helm-core "1.7.8") (migemo "1.9") (cl-lib "0.5"))
 ;; Created: 2009-04-13
 ;; Keywords: matching, convenience, tools, i18n
 ;; URL: https://github.com/emacs-jp/helm-migemo
